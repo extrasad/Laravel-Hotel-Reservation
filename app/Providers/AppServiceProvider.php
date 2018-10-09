ae5b1ef61a07264fe8bff0eaa4a418b63f06aa5e
Carlos Rivas <carlosr1765@gmail.com>
2018-10-09 17:04:30 -0400

    feat(tables): added few tables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema; //Import Schema
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //Solved by increasing StringLength
     }
 
     /**
