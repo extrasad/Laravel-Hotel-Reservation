3896a20185cf72aaae7df0282ed898f14fb334b9
abdiangel <abdiangel.30@gmail.com>
2018-11-15 14:39:19 -0400

    feat(empleados): add empleados layout

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -1,99 +1,81 @@
 @extends('layouts.app')
 
-
 @section('content')
 
-    <div class="row">
+<div class="block-header">
+    <h2>AGREGAR EMPLEADO</h2>
+</div>
 
-        <div class="col-lg-12 margin-tb">
+<div class="row">
 
-            <div class="pull-left">
+    <div class="col-lg-12 m-b-20">
 
-                <h2>Agregar nuevo empleado</h2>
-
-            </div>
-
-            <div class="pull-right">
-
-                <a class="btn btn-primary" href="{{ route('empleados.index') }}"> Atras</a>
-
-            </div>
-
+        <div class="pull-right">
+            
+            <a class="btn btn-primary" href="{{ route('empleados.index') }}"> Atras</a>
+        
         </div>
 
     </div>
 
+</div>
 
-    @if ($errors->any())
-
-        <div class="alert alert-danger">
-
-            <strong>Whoops!</strong> Hay algunos problemas con los datos ingresados.<br><br>
-
-            <ul>
-
-                @foreach ($errors->all() as $error)
-
-                    <li>{{ $error }}</li>
-
-                @endforeach
-
-            </ul>
-
-        </div>
-
-    @endif
-
-
-    <form action="{{ route('empleados.store') }}" method="POST">
-
-    	@csrf
-
-
-         <div class="row">
-
-		    <div class="col-xs-12 col-sm-12 col-md-12">
-
-		        <div class="form-group">
-
-		            <strong>Cedula de Identidad:</strong>
-
-		            <input type="text" name="ci" class="form-control" placeholder="Cedula de Identidad">
-
-		        </div>
-
-		    </div>
-
-		    <div class="col-xs-12 col-sm-12 col-md-12">
-
-		        <div class="form-group">
-
-		            <strong>Nombre:</strong>
-
-		            <input type="text" name="nombre" class="form-control" placeholder="Nombre">
-
-		        </div>
-
-		    </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12">
-
-                <div class="form-group">
-
-                    <strong>Turnos:</strong>
-
-                    {!! Form::select('turnos[]', $turnos,[], array('class' => 'form-control','multiple')) !!}
-
-                </div>
-
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    Creación de empleado
+                </h2>
             </div>
+            <div class="body">
+                @if ($errors->any())
 
-		    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <div class="alert alert-danger">
+            
+                        <strong>Whoops!</strong> Hay algunos problemas con los datos ingresados.<br><br>
+            
+                        <ul>
+            
+                            @foreach ($errors->all() as $error)
+            
+                                <li>{{ $error }}</li>
+            
+                            @endforeach
+            
+                        </ul>
+            
+                    </div>
+            
+                @endif
 
-		            <button type="submit" class="btn btn-primary">Enviar</button>
+                <form action="{{ route('empleados.store') }}" method="POST">
+                    @csrf
 
-		    </div>
-
-    </form>
+                    <label for="ci">Cédula de identidad</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <input type="text" id="ci" name="ci" class="form-control" placeholder="Cedula de Identidad">
+                        </div>
+                    </div>
+                    <label for="nombre">Nombre</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre">
+                        </div>
+                    </div>
+                    <label for="turnos">Turnos</label>
+                    <div class="form-group">
+                        {!! Form::select('turnos[]', $turnos,[], array('class' => 'form-control','multiple', 'id' => 'turnos')) !!}
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary waves-effect">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
