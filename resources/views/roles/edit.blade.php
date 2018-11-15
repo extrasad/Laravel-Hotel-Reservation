52b671f1629cb645ed67f256c4176f6e43e5af9d
abdiangel <abdiangel.30@gmail.com>
2018-11-15 13:04:13 -0400

    feat(roles): add layout to roles

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -3,95 +3,81 @@
 
 @section('content')
 
+<div class="block-header">
+    <h2>EDITAR ROL</h2>
+</div>
+
 <div class="row">
 
-    <div class="col-lg-12 margin-tb">
-
-        <div class="pull-left">
-
-            <h2>Editar Rol</h2>
-
-        </div>
+    <div class="col-lg-12 m-b-20">
 
         <div class="pull-right">
-
+            
             <a class="btn btn-primary" href="{{ route('roles.index') }}"> Atras</a>
-
+        
         </div>
 
     </div>
 
 </div>
 
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    Creación de usuario
+                </h2>
+            </div>
+            <div class="body">
+                
+                @if (count($errors) > 0)
 
-@if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                
+                        <strong>Whoops!</strong> Hay algunos problemas con los datos ingresados.<br><br>
+                
+                        <ul>
+                
+                        @foreach ($errors->all() as $error)
+                
+                            <li>{{ $error }}</li>
+                
+                        @endforeach
+                
+                        </ul>
+                
+                    </div>
+                
+                @endif
 
-    <div class="alert alert-danger">
+                {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
+                    <label for="name">Nombre</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            {!! Form::text('name', null, array('placeholder' => 'Ingrese nombre del rol','class' => 'form-control')) !!}
+                        </div>
+                    </div>
+                    <label>Permisos</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            @foreach($permission as $value)
 
-        <strong>Whoops!</strong> Hay algunos problemas con los datos ingresados.<br><br>
+                            {{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'filled-in', 'id' => 'input-'.$value->id)) }}
+                            <label for="input-{{$value->id}}" class="">{{ $value->name}}</label>
+                
+                            </br>
+                            @endforeach
+                        </div>
+                    </div>
 
-        <ul>
-
-        @foreach ($errors->all() as $error)
-
-            <li>{{ $error }}</li>
-
-        @endforeach
-
-        </ul>
-
-    </div>
-
-@endif
-
-
-{!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
-
-<div class="row">
-
-    <div class="col-xs-12 col-sm-12 col-md-12">
-
-        <div class="form-group">
-
-            <strong>Nombre:</strong>
-
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary waves-effect">Editar rol</button>
+                    </div>
+                {!! Form::close() !!}
+            </div>
         </div>
-
     </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-12">
-
-        <div class="form-group">
-
-            <strong>Permiso:</strong>
-
-            <br/>
-
-            @foreach($permission as $value)
-
-                <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
-
-                {{ $value->name }}</label>
-
-            <br/>
-
-            @endforeach
-
-        </div>
-
-    </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-
-        <button type="submit" class="btn btn-primary">Enviar</button>
-
-    </div>
-
 </div>
-
-{!! Form::close() !!}
-
 
 @endsection
--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 
 @section('content')
@@ -58,6 +58,61 @@
 
     </div>
 
+</div>
+
+@endsection --}}
+
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="block-header">
+    <h2>INFORMACION DE ROL</h2>
+</div>
+
+<div class="row">
+
+    <div class="col-lg-12 margin-tb">
+
+        <div class="pull-right">
+
+            <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
+
+        </div>
+
+    </div>
+
+</div>
+<div class="row clearfix">
+    <!-- Basic Examples -->
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                        {{ $role->name }}
+                    <small>Información detallada del rol {{ $role->name }}</small>
+                </h2>
+            </div>
+            <div class="body">
+                <ul class="list-group">
+                    <li class="list-group-item"><strong class="font-bold col-orange">Nombre de rol:</strong> {{ $role->name }} </li>
+                    <li class="list-group-item">
+                        <strong class="font-bold col-orange">Permisos:</strong>             
+                        @if(!empty($rolePermissions))
+
+                            @foreach($rolePermissions as $v)
+
+                                <label class="label label-success">{{ $v->name }},</label>
+
+                            @endforeach
+
+                        @endif
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
