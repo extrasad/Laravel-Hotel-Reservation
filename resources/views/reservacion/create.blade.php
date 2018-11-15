a656e5c013b0a85ae40a33c62d12a500b6b60444
abdiangel <abdiangel.30@gmail.com>
2018-11-15 16:18:56 -0400

    feat(reservacio): add reservacion layout to half

--- resources/views/reservacion/create.blade.php
+++ resources/views/reservacion/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 
 @section('content')
@@ -131,5 +131,108 @@
 		    </div>
 
     {!! Form::close() !!}
+
+@endsection --}}
+
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="block-header">
+    <h2>AGREGAR RESERVACIÓN</h2>
+</div>
+
+<div class="row">
+
+    <div class="col-lg-12 m-b-20">
+
+        <div class="pull-right">
+            
+            <a class="btn btn-primary" href="{{ route('reservacion.index') }}"> Atras</a>
+        
+        </div>
+
+    </div>
+
+</div>
+
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    Creación de producto
+                </h2>
+            </div>
+            <div class="body">
+                @if ($errors->any())
+
+                    <div class="alert alert-danger">
+            
+                        <strong>Whoops!</strong> Hay algunos problemas con los datos ingresados.<br><br>
+            
+                        <ul>
+            
+                            @foreach ($errors->all() as $error)
+            
+                                <li>{{ $error }}</li>
+            
+                            @endforeach
+            
+                        </ul>
+            
+                    </div>
+            
+                @endif
+
+                {!! Form::open(array('route' => 'reservacion.store','method'=>'POST')) !!}
+                    @csrf
+
+                    <label for="observacion">Observación</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <textarea id="observacion" class="form-control" style="height:150px" name="observacion" placeholder="Observación"></textarea>
+                        </div>
+                    </div>
+                    <label for="cliente1">CI Cliente</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <input type="text" id="cliente1" class="form-control" name="cliente1"></input>
+                        </div>
+                    </div>
+                    <label for="cliente2">CI Acompañante</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <input type="text" id="cliente2" class="form-control" name="cliente2"></input>
+                        </div>
+                    </div>
+                    <label for="auto">Placa</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <input type="text" id="auto" class="form-control" name="auto"></input>
+                        </div>
+                    </div>
+                    <label>Habitación</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            {!! Form::select('habitacion', $habitaciones, array('class' => 'form-control')); !!}
+                        </div>
+                    </div>
+                    <label for="bservacion">Estado</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            {!! Form::select('estado', ['Activa' => 'Activa', 'Inactiva' => 'Inactiva'], array('class' => 'form-control')); !!}
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary waves-effect">Enviar</button>
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
--- resources/views/reservacion/index.blade.php
+++ resources/views/reservacion/index.blade.php
@@ -1,133 +1,132 @@
 @extends('layouts.app')
 
-
 @section('content')
 
-    <div class="row">
+<div class="block-header">
+    <h2>RESERVACIONES</h2>
+</div>
 
-        <div class="col-lg-12 margin-tb">
-
-            <div class="pull-left">
-
-                <h2>Reservaciones</h2>
-
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    Tabla de reservaciones
+                    <small>Esta sección permite crear y observar los actuales reservaciones registradas del sistema.</small>
+                </h2>
             </div>
 
-            <div class="pull-right">
+
+            <div class="body table-responsive">
+
+                @if ($message = Session::get('success'))
+
+                    <div class="alert alert-success">
+            
+                        <p>{{ $message }}</p>
+            
+                    </div>
+            
+                @endif
 
                 @can('reservacion-create')
 
-                <a class="btn btn-success" href="{{ route('reservacion.create') }}"> Crear una nueva reservacion</a>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 align-right">
+                            <a class="btn btn-success" href="{{ route('reservacion.create') }}"> Crear una nueva reservacion</a>
+                        </div>
+                    </div>
 
                 @endcan
 
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>ID</th>
+                
+                            <th>Estado</th>
+                
+                            <th>Consumo</th>
+                
+                            <th>Cliente</th>
+                
+                            <th>Acompañante</th>
+                
+                            <th>Auto</th>
+                
+                            <th>Habitacion</th>
+                
+                            <th>Costo</th>
+                
+                            <th>Acción</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($reservaciones as $reservacion)
+                        <tr>
+                
+                            <td>{{ ++$i }}</td>
+                
+                            <th>{{ $reservacion->id }}</th>
+                
+                            <th>{{ $reservacion->estado }}</th>
+                
+                            <th>@if(isset($reservacion->consumo->costo))
+                            {{ $reservacion->consumo->costo}}
+                            @endif
+                            </th>
+                
+                            <th>{{ $reservacion->cliente1->ci }}</th>
+                
+                            <th>{{ $reservacion->cliente2->ci }}</th>
+                
+                            <th>{{ $reservacion->auto->placa }}</th>
+                
+                            <th>{{ $reservacion->habitacion->habitacion }}</th>
+                
+                            <th>{{ $reservacion->costo }}</th>
+                
+                
+                            <td>
+                
+                                <form action="{{ route('reservacion.destroy',$reservacion->id) }}" method="POST">
+                
+                                    <a class="btn btn-info" href="{{ route('reservacion.show',$reservacion->id) }}">Mostrar</a>
+                
+                                    <a href="{{ route('reservacion.pdf',$reservacion->id) }}" class="btn btn-sm btn-primary">Descargar Factura en PDF</a>
+                
+                                    @can('reservacion-edit')
+                
+                                    <a class="btn btn-primary" href="{{ route('reservacion.edit',$reservacion->id) }}">Editar</a>
+                
+                                    @endcan
+                
+                
+                                    @csrf
+                
+                                    @method('DELETE')
+                
+                                    @can('reservacion-delete')
+                
+                                    <button type="submit" class="btn btn-danger">Borrar</button>
+                
+                                    @endcan
+                
+                                </form>
+                
+                            </td>
+                
+                        </tr>
+                
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-
         </div>
-
     </div>
+</div>
 
-
-    @if ($message = Session::get('success'))
-
-        <div class="alert alert-success">
-
-            <p>{{ $message }}</p>
-
-        </div>
-
-    @endif
-
-
-    <table class="table table-bordered">
-
-        <tr>
-
-            <th>No</th>
-
-            <th>ID</th>
-
-            <th>Estado</th>
-
-            <th>Consumo</th>
-
-            <th>Cliente</th>
-
-            <th>Acompañante</th>
-
-            <th>Auto</th>
-
-            <th>Habitacion</th>
-
-            <th>Costo</th>
-
-            <th width="280px">Acción</th>
-
-        </tr>
-
-	    @foreach ($reservaciones as $reservacion)
-
-	    <tr>
-
-	        <td>{{ ++$i }}</td>
-
-            <th>{{ $reservacion->id }}</th>
-
-            <th>{{ $reservacion->estado }}</th>
-
-            <th>@if(isset($reservacion->consumo->costo))
-            {{ $reservacion->consumo->costo}}
-            @endif
-            </th>
-
-            <th>{{ $reservacion->cliente1->ci }}</th>
-
-            <th>{{ $reservacion->cliente2->ci }}</th>
-
-            <th>{{ $reservacion->auto->placa }}</th>
-
-            <th>{{ $reservacion->habitacion->habitacion }}</th>
-
-            <th>{{ $reservacion->costo }}</th>
-
-
-	        <td>
-
-                <form action="{{ route('reservacion.destroy',$reservacion->id) }}" method="POST">
-
-                    <a class="btn btn-info" href="{{ route('reservacion.show',$reservacion->id) }}">Mostrar</a>
-
-                    <a href="{{ route('reservacion.pdf',$reservacion->id) }}" class="btn btn-sm btn-primary">Descargar Factura en PDF</a>
-
-                    @can('reservacion-edit')
-
-                    <a class="btn btn-primary" href="{{ route('reservacion.edit',$reservacion->id) }}">Editar</a>
-
-                    @endcan
-
-
-                    @csrf
-
-                    @method('DELETE')
-
-                    @can('reservacion-delete')
-
-                    <button type="submit" class="btn btn-danger">Borrar</button>
-
-                    @endcan
-
-                </form>
-
-	        </td>
-
-	    </tr>
-
-	    @endforeach
-
-    </table>
-
-
-    {!! $reservaciones->links() !!}
-
+{!! $reservaciones->links() !!}
 
 @endsection
